670a5a7de86bf04dd8044b28f9c1598b0785c320
Karin Touchie <ktouchie@gmail.com>
2015-12-11 16:11:42 +0100

    Duplicate Exercice2, change prompts, variables

---
+++ Exercice3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Exercice3 {
+    
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 1;
+        int total = 0;
+        while (num>=1) {            
+            System.out.println("Veuillez saisir un nombre entier positif:");
+            num = sc.nextInt();
+            if ((num%3)==0) {
+                System.out.println(num + " est multiple de 3.");
+                total = (total+1)
+            }
+        }
+        
+        System.out.println("Vous avez saisi " + total + " multiple(s) de 3 en total.");               
+    }
+}
